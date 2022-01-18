9ce75d4d1b4b87d51bda65183c75e5a0aa6fc4c3
tanujsahney <tanuj.sahney24@gmail.com>
2022-01-18 22:15:52 +0530

    Tab component

--- frontend/components/tabs.tsx
+++ frontend/components/tabs.tsx
@@ -1,7 +1,10 @@
+import { Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Tab from "@material-ui/core/Tab/Tab";
 import Tabs from "@material-ui/core/Tabs/Tabs";
 import React from "react";
+import BasicModal from "./BasicModal";
+import NFTCardGrid from "./NFTCardGrid";
 
 const useStyles = makeStyles({
   tab: {
@@ -10,6 +13,19 @@ const useStyles = makeStyles({
   },
 });
 
+const notes = [
+  "500",
+  "50",
+  "5000",
+  "500",
+  "50",
+  "5000",
+  "500",
+  "50",
+  "5000",
+  "15",
+];
+
 const HomePageTabs = () => {
   const classes = useStyles();
 
@@ -17,22 +33,34 @@ const HomePageTabs = () => {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  let tabs_array = [
+    <NFTCardGrid notes={notes} />,
+    <NFTCardGrid notes={notes} />,
+    <BasicModal />,
+  ];
 
   return (
-    <Tabs
-      value={value}
-      onChange={handleChange}
-      centered
-      TabIndicatorProps={{
-        style: { backgroundColor: "green" },
-      }}
-    >
-      <Tab label="All NFTs" className={classes.tab} />
-      <Tab label="Sold NFTs" className={classes.tab} />
-      <Tab label="DAO" className={classes.tab} />
-      <Tab label="Owned Tokens" className={classes.tab} />
-      <Tab label="Owned NFTs" className={classes.tab} />
-    </Tabs>
+    <>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        centered
+        TabIndicatorProps={{
+          style: { backgroundColor: "green" },
+        }}
+      >
+        <Tab label="All NFTs" className={classes.tab}>
+          <NFTCardGrid notes={notes} />
+        </Tab>
+        <Tab label="Sold NFTs" className={classes.tab}>
+          <p>abcg</p>
+        </Tab>
+        <Tab label="DAO" className={classes.tab} />
+        <Tab label="Owned Tokens" className={classes.tab} />
+        <Tab label="Owned NFTs" className={classes.tab} />
+      </Tabs>
+      <Paper>{tabs_array[value]}</Paper>
+    </>
   );
 };
 
