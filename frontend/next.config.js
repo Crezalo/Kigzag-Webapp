feb11ea4440a83e05242b52bf5a8bc18572eacc2
Ronnie Gandhi <ronniegandhi19999@gmail.com>
2022-03-03 17:21:25 +0530

    production ready build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,4 +10,9 @@ module.exports = {
     domains: ["ipfs.io", "ik.imagekit.io", "localhost", "d19bos5qssbslz.cloudfront.net", "kigzag-video-thumbnail.s3.eu-north-1.amazonaws.com"],
     formats: ["image/webp"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
