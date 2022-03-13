4e7ca20b32f79f05c151b639620e33b55b6bc029
Ronnie Gandhi <ronniegandhi19999@gmail.com>
2022-03-13 17:30:44 +0530

    disable swcMinify to decrease runtime build memory

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -14,7 +14,7 @@ dotenvLoad();
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   images: {
     domains: ["ipfs.io", "ik.imagekit.io", "localhost", "d19bos5qssbslz.cloudfront.net", "kigzag-video-thumbnail.s3.eu-north-1.amazonaws.com"],
     formats: ["image/webp"],
