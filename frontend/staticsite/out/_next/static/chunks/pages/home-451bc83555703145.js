8abfeaa04b30a4e9ff344b16afe93b0209867def
Ronnie Gandhi <ronniegandhi19999@gmail.com>
2023-04-23 23:26:34 +0530

    last commit build fee

---
+++ frontend/staticsite/out/_next/static/chunks/pages/home-451bc83555703145.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[229],{94309:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/home",function(){return u(17448)}])}},function(n){n.O(0,[6319,1388,9565,3461,1762,9418,7053,286,4054,6374,9794,7448,9774,2888,179],function(){return n(n.s=94309)}),_N_E=n.O()}]);
