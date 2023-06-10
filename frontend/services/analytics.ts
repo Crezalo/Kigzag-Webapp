d7404a42a04ab3e41b8a4a83e903d99e6862bb6a
Ronnie Gandhi <ronniegandhi19999@gmail.com>
2023-06-11 03:59:50 +0530

    adding google analytics FE debug with build files

--- frontend/services/analytics.ts
+++ frontend/services/analytics.ts
@@ -1,7 +1,9 @@
 import ReactGA from "react-ga";
 
 export const initGA = () => {
-  ReactGA.initialize(process.env.NEXT_STATIC_GOOGLE_ANALYTICS_TAG);
+  ReactGA.initialize(process.env.NEXT_STATIC_GOOGLE_ANALYTICS_TAG, {
+    debug: true,
+  });
 };
 
 export const logPageView = (url: string) => {
